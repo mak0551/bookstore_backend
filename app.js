0edba7388793249cab366691035bcbc0640b26ad
mak0551 <khanafroz1516@gmail.com>
2024-09-27 02:01:05 +0530

    setting port

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require("express"); // requiring express
 const app = express(); // assigning express to app
 const mongoose = require("mongoose"); // requiring mongoose
-const config = require('./config'); // importing database connection string
+const config = require("./config"); // importing database connection string
 const bookroutes = require("./routes/bookroutes"); // requiring book routes
 const { basicAuth, tokenAuth } = require("./auth/auth"); // requiring basicauth
 const userroutes = require("./routes/userroutes"); // requiring user routes
 const cors = require("cors"); // requiring cors policy
 
-
 // middleware for handling CORS policy here we have 2 options
 // 1st option allows all origins with default of cors(*) // aur ye hamesha routes k upper rehna re bhai nai toh bulle miljate
-app.use(cors()); 
+app.use(cors());
 // 2nd option allow custom origins, it is better because we can have controls
 // app.use(
 //   cors({
@@ -36,16 +35,11 @@ app.get("/", (req, res) => {
   res.send("hi");
 });
 
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("app listening on port 3000!"));
+
 //ya se mongodb ka kaam shuru
 mongoose
   .connect(config.dbconstr)
-  .then(() => {
-    console.log("app connected");
-    app.listen(3000, () => {
-      console.log("app listening on port 3000!");
-    });
-  })
-  .catch((error) => {
-    // console.log(error);
-    console.log("error connecting database");
-  });
+  .then(() => console.log("app connected"))
+  .catch((error) => console.log("error connecting database"));
